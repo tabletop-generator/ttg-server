3a760317d67da6c0b4c7ee3e90a102dbe90ef1f3
Uday R <82779467+uday-rana@users.noreply.github.com>
2024-11-19 04:47:23 -0500

    feat: add response generator functions, move routes into /routes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const express = require("express");
 const helmet = require("helmet");
 const { pinoHttp } = require("pino-http");
-const { version } = require("../package.json");
 const logger = require("./logger");
 
 const pino = pinoHttp({
@@ -26,20 +25,8 @@ app.use(cors());
 // Use gzip/deflate compression middleware
 app.use(compression());
 
-// Define a simple health check route. If the server is running
-// we'll respond with a 200 OK.  If not, the server isn't healthy.
-app.get("/", (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader("Cache-Control", "no-cache");
-
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: "ok",
-    githubUrl: "https://github.com/tabletop-generator/server/",
-    version,
-  });
-});
+// Define our routes
+app.use("/", require("./routes"));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
