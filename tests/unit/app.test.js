019e71381299e4bc4e8700e90cd59f68e10ce5f1
Uday R <82779467+uday-rana@users.noreply.github.com>
2024-11-19 05:50:59 -0500

    test: update 404 tests to assert valid error response format

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -3,6 +3,16 @@ const request = require("supertest");
 const app = require("../../src/app");
 
 describe("404 handler", () => {
-  test("should return HTTP 404 response for nonexistent route", () =>
-    request(app).get("/notarealendpoint").expect(404));
+  test("should return HTTP 404 response for nonexistent route", async () => {
+    const response = await request(app).get("/notarealendpoint");
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toStrictEqual({
+      status: "error",
+      error: {
+        code: 404,
+        message: "not found",
+      },
+    });
+  });
 });
