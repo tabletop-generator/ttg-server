6e8ae6218a4954cfe067e762999334898d97a0f9
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-01-15 22:33:36 -0500

    chore: fix wrong limit rate in comment

--- src/limiter.js
+++ src/limiter.js
@@ -2,7 +2,7 @@ const { rateLimit } = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
