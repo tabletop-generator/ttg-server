2b65e859132a59a917add34e9fcc0edd0afd5c56
Uday R <82779467+uday-rana@users.noreply.github.com>
2025-01-15 20:37:35 -0500

    feat: empty /collections endpoints

---
+++ src/routes/api/collections/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+/**
+ * The entry-point for /collections endpoints
+ */
+
+const router = express.Router();
+
+router.post(`/`, require("./post"));
+
+router.get(`/`, require("./get"));
+
+router.get(`/:collectionId`, require("./get"));
+
+router.patch(`/:collectionId`, require("./patchById"));
+
+router.delete(`/:collectionId`, require("./deleteById"));
+
+module.exports = router;
