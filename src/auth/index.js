d334ed0faac3280eeb759186e11f8904f7e7faf2
Uday R <82779467+uday-rana@users.noreply.github.com>
2025-01-15 21:42:27 -0500

    feat: implement amazon cognito auth

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,5 +1,9 @@
+// Prefer Amazon Cognito
+if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+  module.exports = require("./cognito");
+}
 // Allow for an .htpasswd file to be used, but not in production
-if (process.env.HTPASSWD_FILE && process.NODE_ENV !== "production") {
+else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== "production") {
   module.exports = require("./basic-auth");
 }
 // In all other cases, we need to stop now and fix our config
