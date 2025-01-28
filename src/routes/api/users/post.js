b6cb1d4e17aad3b08cc6a2e066964b5ccc285768
Sangjune Lee <130009892+SJLEE411@users.noreply.github.com>
2025-01-28 01:03:32 -0500

    commit as review for src/routes/api/users/post.js
    Co-authored-by: Uday Rana <82779467+uday-rana@users.noreply.github.com>

--- src/routes/api/users/post.js
+++ src/routes/api/users/post.js
@@ -23,24 +23,32 @@ module.exports = async (req, res, next) => {
   }
 
   // If user doesn't exist, create them
-  if (!existingUser) {
-    try {
-      await prisma.user.create({
-        data: {
-          hashedEmail: req.user,
-          displayName: `user_${req.user.slice(0, 8)}`, // Default display name using first 8 chars of hash
-          profileBio: "", // Empty bio by default
-        },
-      });
-      logger.info({ userId: req.user }, "Created new user record");
-    } catch (err) {
-      logger.error({ err }, "Error creating new user");
-      return next(err);
-    }
-  } else {
+    // Create URL for Location header for GET /users/:id
+    const locationURL = new URL(
+      `/v1/users/${req.user}`,
+      process.env?.API_URL || `https://${req.headers?.host}`,
+    );
+    
+  if (existingUser) {
     logger.debug({ userId: req.user }, "User record already exists");
+    return res.status(200).set("Location", locationURL).json(createSuccessResponse({user: existingUser}));
+  }
+  
+  // User record doesn't exist, create one
+  let newUser;
+  try {
+    newUser = await prisma.user.create({
+      data: {
+        hashedEmail: req.user,
+        displayName: `user_${req.user.slice(0, 8)}`, // Default display name using first 8 chars of hash
+        profileBio: "", // Empty bio by default
+      },
+    });
+    logger.info({ userId: req.user }, "Created new user record");
+  } catch (err) {
+    logger.error({ err }, "Error creating new user");
+    return next(err);
   }
 
-  // Return success response
-  return res.status(201).json(createSuccessResponse());
+  return res.status(201).set("Location", locationURL).json(createSuccessResponse({user: newUser}));
 };
