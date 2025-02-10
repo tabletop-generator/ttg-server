52aaf54099248dc642a944285e48079fc61270d3
cyan <vatsalbhatt2002@gmail.com>
2025-02-10 12:08:18 -0500

    Implemetd GET /v1/users/:id

--- src/routes/api/users/getById.js
+++ src/routes/api/users/getById.js
@@ -1,16 +1,52 @@
 const logger = require("../../../logger");
-const { createSuccessResponse } = require("../../../response");
+const {
+  createSuccessResponse,
+  createErrorResponse,
+} = require("../../../response");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 /**
  * Get a user by their id
  */
+module.exports = async (req, res) => {
+  const userId = parseInt(req.params.userId, 10);
 
-// eslint-disable-next-line no-unused-vars
-module.exports = async (req, res, next) => {
+  if (isNaN(userId)) {
+    return res.status(400).json(createErrorResponse(400, "Invalid user ID"));
+  }
+
   logger.debug(
     { user: req.user, id: req.params.id },
     `received request: GET /v1/users/:userId`,
   );
 
-  return res.status(418).json(createSuccessResponse());
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        hashedEmail: true,
+        joinDate: true,
+        displayName: true,
+        profileBio: true,
+        profilePictureUrl: true,
+        profilePictureUrlExpiry: true,
+        assets: true,
+        collections: true,
+        comments: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json(createErrorResponse(404, "User not found"));
+    }
+
+    return res.status(200).json(createSuccessResponse(user));
+  } catch (error) {
+    logger.error(error, "Error fetching user");
+    return res
+      .status(500)
+      .json(createErrorResponse(500, "Internal server error"));
+  }
 };
