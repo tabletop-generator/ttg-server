c90e64c20b8cb4626b9c57f93fe27fb20db4785f
Uday R <82779467+uday-rana@users.noreply.github.com>
2025-02-17 17:43:32 -0500

    Create getAsset, simplify saveAsset

--- src/model/asset.js
+++ src/model/asset.js
@@ -8,48 +8,56 @@ const baseSchema = z.object({
   visibility: z.enum(["public", "private", "unlisted"]),
 });
 
-const characterSchema = z.object({
-  name: z.string(),
-  race: z.enum([
-    "human",
-    "elf",
-    "drow",
-    "half_elf",
-    "half_orc",
-    "halfling",
-    "dwarf",
-    "gnome",
-    "tiefling",
-    "githyanki",
-    "dragonborn",
-  ]),
-  class: z.enum([
-    "barbarian",
-    "bard",
-    "cleric",
-    "druid",
-    "fighter",
-    "monk",
-    "paladin",
-    "ranger",
-    "rogue",
-    "sorcerer",
-    "warlock",
-    "wizard",
-  ]),
-  gender: z.enum(["male", "female", "non_binary", "genderfluid", "agender"]),
-  alignment: z.enum([
-    "lawful_good",
-    "neutral_good",
-    "chaotic_good",
-    "lawful_neutral",
-    "true_neutral",
-    "chaotic_neutral",
-    "lawful_evil",
-    "neutral_evil",
-    "chaotic_evil",
-  ]),
-});
+const characterSchema = z
+  .object({
+    name: z.string(),
+    race: z.enum([
+      "human",
+      "elf",
+      "drow",
+      "half_elf",
+      "half_orc",
+      "halfling",
+      "dwarf",
+      "gnome",
+      "tiefling",
+      "githyanki",
+      "dragonborn",
+    ]),
+    class: z.enum([
+      "barbarian",
+      "bard",
+      "cleric",
+      "druid",
+      "fighter",
+      "monk",
+      "paladin",
+      "ranger",
+      "rogue",
+      "sorcerer",
+      "warlock",
+      "wizard",
+    ]),
+    gender: z.enum(["male", "female", "non_binary", "genderfluid", "agender"]),
+    alignment: z.enum([
+      "lawful_good",
+      "neutral_good",
+      "chaotic_good",
+      "lawful_neutral",
+      "true_neutral",
+      "chaotic_neutral",
+      "lawful_evil",
+      "neutral_evil",
+      "chaotic_evil",
+    ]),
+    abilities: z.string().optional(),
+    appearance: z.string().optional(),
+    background: z.string().optional(),
+    equipment: z.string().optional(),
+    motivation: z.string().optional(),
+    personality: z.string().optional(),
+  })
+  .strict();
 
 const fullSchema = baseSchema
   .extend({
@@ -89,42 +97,56 @@ async function saveAsset(
 
   // Save asset to database based on the type
   let asset;
-  switch (metadata.type) {
-    case "character":
-      asset = await prisma.asset.create({
-        data: {
-          uuid: assetId,
-          creatorId: userId,
-          name: data.name,
-          type: type,
-          visibility: visibility,
-          description: description,
-          imageUrl: url,
-          imageUrlExpiry: urlExpiry,
-          character: {
-            create: {
-              race: data.race,
-              class: data.class,
-              gender: data.gender,
-              alignment: data.alignment,
-              appearance: data.appearance,
-              abilities: data.abilities,
-              background: data.background,
-              equipment: data.equipment,
-              motivation: data.motivation,
-              personality: data.personality,
-            },
-          },
-        },
-      });
-      break;
+  asset = await prisma.asset.create({
+    data: {
+      uuid: assetId,
+      creatorId: userId,
+      name: data.name,
+      type: type,
+      visibility: visibility,
+      description: description,
+      imageUrl: url,
+      imageUrlExpiry: urlExpiry,
+      [type]: {
+        create: { ...data },
+      },
+    },
+  });
 
-    default:
-      break;
+  return asset;
+}
+
+async function getAsset(assetUuid) {
+  // Get asset from database based on the type
+  let asset = await prisma.asset.findUniqueOrThrow({
+    where: { uuid: assetUuid },
+    include: {
+      character: true,
+    },
+  });
+
+  // If image url is expired, create and save new url before returning
+  if (asset.imageUrlExpiry.getTime() <= new Date().getTime()) {
+    const key = `${asset.creatorId}/${asset.id}`;
+    const { url, urlExpiry } = await createPresignedUrl(key);
+
+    asset = prisma.asset.update({
+      where: {
+        id: asset.id,
+      },
+      data: {
+        imageUrl: url,
+        imageUrlExpiry: urlExpiry,
+      },
+      include: {
+        character: true,
+      },
+    });
   }
 
   return asset;
 }
 
 module.exports.save = saveAsset;
+module.exports.get = getAsset;
 module.exports.schema = fullSchema;
