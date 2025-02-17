d17dedd6d583843c3e49661ef443daf8e38d3112
cyan <vatsalbhatt2002@gmail.com>
2025-02-16 19:28:03 -0500

    suggestions have been implemented

--- src/routes/api/users/getById.js
+++ src/routes/api/users/getById.js
@@ -1,57 +1,45 @@
 const logger = require("../../../logger");
-const hash = require("../../../hash");
 const validator = require("validator");
-const {
-  createSuccessResponse,
-  createErrorResponse,
-} = require("../../../response");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = require("../../../model/data/prismaClient");
+const { createSuccessResponse } = require("../../../response");
 
 /**
  * Get a user by their id
  */
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
   logger.debug(
     { user: req.user, id: req.params.id },
     `received request: GET /v1/users/:userId`,
   );
 
-  const email = req.params.userId;
-  if (!validator.isEmail(email)) {
-    return res
-      .status(400)
-      .json(createErrorResponse(400, "Invalid email format"));
+  const userId = req.params.userId;
+  if (!validator.isHash(userId, "sha256")) {
+    return next({ status: 400, message: "Invalid hash" });
   }
 
-  const userId = hash(email).toString();
+  let user;
 
   try {
-    const user = await prisma.user.findUnique({
+    user = await prisma.user.findUnique({
       where: { hashedEmail: userId }, //searching prisma for the userId (hashed email)
-      select: {
-        id: true,
-        hashedEmail: true,
-        joinDate: true,
-        displayName: true,
-        profileBio: true,
-        profilePictureUrl: true,
-        profilePictureUrlExpiry: true,
+      include: {
         assets: true,
         collections: true,
         comments: true,
       },
     });
-
-    if (!user) {
-      return res.status(404).json(createErrorResponse(404, "User not found"));
-    }
-
-    return res.status(200).json(createSuccessResponse(user));
   } catch (error) {
-    logger.error(error, "Error fetching user");
-    return res
-      .status(500)
-      .json(createErrorResponse(500, "Internal server error"));
+    logger.error({ error }, "Error fetching user");
+    error.status = 500;
+    error.message = "Internal server error";
+    return next(error);
   }
+
+  if (!user) {
+    logger.warn("User not found");
+    return next({ status: 404, message: "User not found" });
+  }
+
+  logger.debug({ user }, `found user`);
+  return res.status(200).json(createSuccessResponse(user));
 };
