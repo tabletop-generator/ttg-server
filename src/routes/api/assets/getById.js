b75de8fa4daf695ff290ce545e29c97952e4980f
Sangjune Lee <130009892+SJLEE411@users.noreply.github.com>
2025-02-18 17:54:24 -0500

    Implement GET /v1/assets/:id (#145)
    * GET /assets endpoint with DB int
    
    * testing complete for the get endpoints with hurl, all passing so far on testdb/assets/sql after running init db
    
    * GET /assets endpoint with DB int
    
    * testing complete for the get endpoints with hurl, all passing so far on testdb/assets/sql after running init db
    
    * init for patch by id
    
    * Fix lint error
    
    * Revert "init for patch by id"
    
    This reverts commit f2419216db6b25c0699438d5e48de8472aef04f0.
    
    * Update prisma client imports
    
    * fixing asset get.js to match prisma schema again and updating test file
    
    * removing testdb
    
    * fixing hurl testing
    
    * fixing hurl testing2
    
    * hur fixing 3
    
    * hurl, get-assets now has posting sample data
    
    * seperating get and getbyid, this only applies to getbyid rn
    
    * getbyid fixes
    
    * rebase fix
    
    * testing complete for the get endpoints with hurl, all passing so far on testdb/assets/sql after running init db
    
    * GET /assets endpoint with DB int
    
    * testing complete for the get endpoints with hurl, all passing so far on testdb/assets/sql after running init db
    
    * init for patch by id
    
    * Fix lint error
    
    * Revert "init for patch by id"
    
    This reverts commit f2419216db6b25c0699438d5e48de8472aef04f0.
    
    * Update prisma client imports
    
    * fixing asset get.js to match prisma schema again and updating test file
    
    * removing testdb
    
    * fixing hurl testing
    
    * fixing hurl testing2
    
    * hur fixing 3
    
    * hurl, get-assets now has posting sample data
    
    * git rebase fix
    
    * Update src/routes/api/assets/getById.js
    
    Co-authored-by: Uday Rana <82779467+uday-rana@users.noreply.github.com>
    
    * Update src/routes/api/assets/getById.js
    
    Co-authored-by: Uday Rana <82779467+uday-rana@users.noreply.github.com>
    
    * fixes after review
    
    * changed made to use asset.get instead of using prisma directly
    
    * removing outer try catch block
    
    * Update src/routes/api/assets/getById.js
    
    Co-authored-by: Uday Rana <82779467+uday-rana@users.noreply.github.com>
    
    * include user in query, allow getting unlisted assets
    
    * Make comment more verbose
    
    * Add test for forbidden access
    
    * Update tests/integration/assets-getbyid.hurl
    
    * Update tests/integration/assets-getbyid.hurl
    
    * Set asset visibility to private in test
    
    * include user even when not generating new profile picture url
    
    * add debug log for found asset
    
    ---------
    
    Co-authored-by: Uday R <82779467+uday-rana@users.noreply.github.com>

--- src/routes/api/assets/getById.js
+++ src/routes/api/assets/getById.js
@@ -1,16 +1,51 @@
 const logger = require("../../../logger");
+const { z } = require("zod");
+const { PrismaClientKnownRequestError } = require("@prisma/client").Prisma;
+const asset = require("../../../model/asset");
 const { createSuccessResponse } = require("../../../response");
 
 /**
- * Get an asset by it's id
+ * Get an asset by its id
  */
-
-// eslint-disable-next-line no-unused-vars
 module.exports = async (req, res, next) => {
   logger.debug(
-    { user: req.user, id: req.params.id },
+    { user: req.user, id: req.params.assetId },
     `received request: GET /v1/assets/:assetId`,
   );
 
-  return res.status(418).json(createSuccessResponse());
+  // Validate UUID format
+  try {
+    z.string().uuid().parse(req.params.assetId);
+  } catch (error) {
+    logger.debug({ error }, "invalid asset ID format");
+    return next({ status: 400, message: "Invalid uuid" });
+  }
+
+  // Get the asset with the associated user
+  let foundAsset;
+  try {
+    foundAsset = await asset.get(req.params.assetId, true);
+    logger.debug({ foundAsset }, "Retrieved asset");
+  } catch (error) {
+    logger.error({ error }, "Error fetching asset");
+
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      return next({ status: 404, message: "Asset not found" });
+    }
+    return next({ status: 500, message: "Internal server error" });
+  }
+
+  // Disallow viewing other users' private assets
+  if (
+    foundAsset.visibility == "private" &&
+    foundAsset.user.hashedEmail !== req.user
+  ) {
+    return next({ status: 403, message: "Forbidden" });
+  }
+
+  // Return the asset
+  return res.status(200).json(createSuccessResponse({ asset: foundAsset }));
 };
