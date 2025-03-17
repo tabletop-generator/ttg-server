626a211eae3919bbd39388908212a0b123ead8fa
cyan <62727940+cyan-1@users.noreply.github.com>
2025-03-17 11:54:24 -0400

    Implement DELETE /v1/assets/:id (#161)
    * implemented delete asset
    
    * the implemantation file
    
    * using correct id for the logger
    
    * consistent errors, checking for owner
    
    * bugged needs fixing
    
    * implemented requested changes
    
    * using already existing instance of prisma
    
    * needs fixing
    
    * applied changes
    
    * Update deleteById.js

--- src/routes/api/assets/deleteById.js
+++ src/routes/api/assets/deleteById.js
@@ -1,16 +1,59 @@
 const logger = require("../../../logger");
+const z = require("zod");
+const { delete: deleteAsset } = require("../../../model/asset");
 const { createSuccessResponse } = require("../../../response");
+const prisma = require("../../../model/data/prismaClient");
 
 /**
- * Delete an asset by it's id
+ * Delete an asset by its id after verifying ownership
  */
-
-// eslint-disable-next-line no-unused-vars
 module.exports = async (req, res, next) => {
   logger.debug(
-    { user: req.user, id: req.params.id },
-    `received request: DELETE /v1/assets/:assetId`,
+    { user: req.user, assetId: req.params.assetId },
+    "received request: DELETE /v1/assets/:assetId",
   );
 
-  return res.status(418).json(createSuccessResponse());
+  const assetId = req.params.assetId;
+
+  try {
+    z.string().uuid().parse(assetId);
+  } catch (error) {
+    logger.warn({ error }, "invalid asset ID format");
+    return next({ status: 400, message: "invalid asset id" });
+  }
+
+  let asset;
+  try {
+    asset = await prisma.asset.findUnique({
+      where: { uuid: assetId },
+      include: {
+        character: true,
+        user: true,
+      },
+    });
+  } catch (error) {
+    logger.error(error, "Error fetching asset");
+    return next({ status: 500, message: "Internal server error" });
+  }
+
+  if (!asset) {
+    logger.warn("Asset not found");
+    return next({ status: 404, message: "Asset not found" });
+  }
+
+  try {
+    await deleteAsset(asset.uuid, req.user);
+  } catch (error) {
+    if (error.code === "P2025") {
+      logger.warn("Asset for the user not found during deletion");
+      return next({ status: 403, message: "Forbidden" });
+    }
+    logger.error(error, "Error deleting asset");
+    return next({ status: 500, message: "Internal server error" });
+  }
+
+  logger.info(asset.uuid, "Asset deleted");
+  return res
+    .status(200)
+    .json(createSuccessResponse({ message: "Asset deleted successfully" }));
 };
