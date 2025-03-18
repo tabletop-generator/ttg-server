2ba3fce8d109c938db21561ba5ca45b743271a88
cyan <62727940+cyan-1@users.noreply.github.com>
2025-03-18 18:09:25 -0400

    Implement DELETE /v1/collections/:id (#166)
    * implement delete collection
    
    * tests :)
    
    * wip
    
    * implemented delete collection by id

--- src/routes/api/collections/deleteById.js
+++ src/routes/api/collections/deleteById.js
@@ -1,16 +1,51 @@
 const logger = require("../../../logger");
+const { z } = require("zod");
 const { createSuccessResponse } = require("../../../response");
+const collection = require("../../../model/collection");
 
-/**
- * Delete a collection by it's id
- */
-
-// eslint-disable-next-line no-unused-vars
 module.exports = async (req, res, next) => {
   logger.debug(
-    { user: req.user, id: req.params.id },
-    `received request: DELETE /v1/collections/:collectionId`,
+    { user: req.user, id: req.params.collectionId },
+    "received request: DELETE /v1/collections/:collectionId",
   );
 
-  return res.status(418).json(createSuccessResponse());
+  const collectionIdParam = req.params.collectionId;
+  const userHash = req.user;
+  let collectionId;
+  try {
+    collectionId = z.coerce.number().int().parse(collectionIdParam);
+  } catch (error) {
+    logger.warn({ error }, "Invalid collection ID format");
+    return next({ status: 400, message: "Invalid collection ID" });
+  }
+
+  try {
+    await collection.get(collectionId, userHash);
+  } catch (error) {
+    logger.warn({ error }, "Error fetching collection");
+    if (error.status === 403) {
+      return next({ status: 403, message: "Forbidden" });
+    } else if (
+      error.code === "P2025" ||
+      (error.message && error.message.includes("No Collection found"))
+    ) {
+      return next({ status: 404, message: "Collection not found" });
+    } else {
+      return next({ status: 500, message: "Internal server error" });
+    }
+  }
+
+  try {
+    await collection.deleteCollection(collectionId, userHash);
+
+    logger.debug(collectionId, "Collection deleted");
+    return res
+      .status(200)
+      .json(
+        createSuccessResponse({ message: "Collection deleted successfully" }),
+      );
+  } catch (error) {
+    logger.error(error, "Error deleting collection");
+    return next({ status: 500, message: "Internal server error" });
+  }
 };
