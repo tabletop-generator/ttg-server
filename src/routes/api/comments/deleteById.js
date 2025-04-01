27bb27c93933eebe8f73bb64329327814084337a
Sangjune Lee <130009892+SJLEE411@users.noreply.github.com>
2025-04-01 11:36:03 -0400

    delete route push (#189)

--- src/routes/api/comments/deleteById.js
+++ src/routes/api/comments/deleteById.js
@@ -1,16 +1,29 @@
 const logger = require("../../../logger");
+const comment = require("../../../model/comment");
 const { createSuccessResponse } = require("../../../response");
 
 /**
- * Delete a comment by it's id
+ * Delete a comment by its id
+ * Both the comment author and the asset owner can delete comments
  */
 
-// eslint-disable-next-line no-unused-vars
 module.exports = async (req, res, next) => {
   logger.debug(
-    { user: req.user, id: req.params.id },
+    { user: req.user, id: req.params.commentId },
     `received request: DELETE /v1/comments/:commentId`,
   );
 
-  return res.status(418).json(createSuccessResponse());
+  const commentId = req.params.commentId;
+
+  try {
+    // Use the comment model to handle delete logic
+    await comment.deleteById(req.user, commentId);
+
+    return res
+      .status(200)
+      .json(createSuccessResponse({ message: "Comment deleted successfully" }));
+  } catch (error) {
+    // Pass error to error handler middleware
+    return next(error);
+  }
 };
