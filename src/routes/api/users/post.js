3744d2672f8bc3fe3e50cb6b7d7eaa9cadfcb050
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-25 23:54:56 -0400

    Create POST /users

--- src/routes/api/users/post.js
+++ src/routes/api/users/post.js
@@ -1,16 +1,48 @@
 const logger = require("../../../lib/logger");
-const { createSuccessResponse } = require("../../../lib/response");
+const prisma = require("../../../model/data/prismaClient");
+const { createErrorResponse } = require("../../../lib/response");
 
 /**
  * Create the current user if they don't already exist
  */
 
-// eslint-disable-next-line no-unused-vars
 module.exports = async (req, res, next) => {
   logger.debug(
     { user: req.user, body: req.body },
     `received request: POST /v1/users`,
   );
 
-  return res.status(418).json(createSuccessResponse());
+  let user;
+
+  // Try to find existing user
+  try {
+    user = await prisma.user.findUnique({
+      where: {
+        userId: req.user,
+      },
+    });
+  } catch (error) {
+    logger.error({ error }, "error finding user");
+    return next(createErrorResponse(500, "Error finding user"));
+  }
+
+  // If found, return their record
+  if (user) {
+    return res.status(200).json(user);
+  }
+
+  // If not, create it
+  try {
+    user = await prisma.user.create({
+      data: {
+        userId: req.user,
+        displayName: `User ${req.user.slice(0, 6)}`,
+      },
+    });
+  } catch (error) {
+    logger.error({ error }, "error creating new user");
+    return next(createErrorResponse(500, "Error creating new user"));
+  }
+
+  return res.status(201).json(user);
 };
