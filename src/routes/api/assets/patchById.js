d9bbe0957c52cdce88967522bae8ad4fe87fa447
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-27 15:35:50 -0400

    Create PATCH /assets/{assetId}

--- src/routes/api/assets/patchById.js
+++ src/routes/api/assets/patchById.js
@@ -1,15 +1,31 @@
+const { updateAsset } = require("../../../model/asset");
+const { createHttpError } = require("../../../lib/error");
 const { logger } = require("../../../lib/logger");
 
 /**
  * Update an asset by it's id
  */
 
-// eslint-disable-next-line no-unused-vars
 module.exports = async (req, res, next) => {
   logger.debug(
     { user: req.user, assetIdParam: req.params.assetId },
     `received request: PATCH /v1/assets/:assetId`,
   );
 
-  return res.status(418);
+  let asset;
+  try {
+    asset = await updateAsset(req.params.assetId, req.user, req.body);
+  } catch (error) {
+    if (error.message === "Not Found") {
+      return next(createHttpError(404, "Not Found"));
+    }
+    if (error.message === "Forbidden") {
+      return next(createHttpError(403, "Forbidden"));
+    }
+
+    logger.error({ error }, "error deleting asset");
+    return next(createHttpError(500, "Error deleting asset"));
+  }
+
+  return res.status(200).json(asset);
 };
